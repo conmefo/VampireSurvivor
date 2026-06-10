6e80ea089103ca43a4b1e780a75661776223decb
conmefo <hoquangminh757@gmail.com>
2026-06-10 15:17:12 +0700

    edit sprite scale and clangd-format

--- src/States/Game/GameState.h
+++ src/States/Game/GameState.h
@@ -5,26 +5,26 @@
 #include "../BaseState.h"
 
 class GameState : public BaseState {
-public:
-  explicit GameState(StateContext context);
-  ~GameState() override = default;
+  public:
+    explicit GameState(StateContext context);
+    ~GameState() override = default;
 
-  void Init() override;
-  void HandleInput(sf::Event &event, sf::RenderWindow &window) override;
-  void Update(float dt) override;
-  void Draw(sf::RenderWindow &window) override;
+    void Init() override;
+    void HandleInput(sf::Event &event, sf::RenderWindow &window) override;
+    void Update(float dt) override;
+    void Draw(sf::RenderWindow &window) override;
 
-private:
-  void UpdateCamera(float dt);
-  void ApplyCameraToView();
+  private:
+    void UpdateCamera(float dt);
+    void ApplyCameraToView();
 
-  static constexpr float CameraSpeed = 200.0f;
-  static constexpr float ViewWidth = 640.0f;
-  static constexpr float ViewHeight = 480.0f;
+    static constexpr float CameraSpeed = 200.0f;
+    static constexpr float ViewWidth = 640.0f;
+    static constexpr float ViewHeight = 480.0f;
 
-  TileMap m_tileMap;
-  sf::View m_worldView;
-  sf::Vector2f m_cameraCenter;
-  EnemyDatabase m_enemyDatabase;
-  EnemyPool m_enemyPool;
+    TileMap m_tileMap;
+    sf::View m_worldView;
+    sf::Vector2f m_cameraCenter;
+    EnemyDatabase m_enemyDatabase;
+    EnemyPool m_enemyPool;
 };
